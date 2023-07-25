f66f8ab63b7950ab11d88b9bd0e8db04da075eb2
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-07-26 00:41:47 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_passwordProtectedProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "54321";
+            string userInput;
+
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.WriteLine("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Секретное слово: ПАРОЛЬ.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Не верный пароль. Осталось попыток - " + (tryCount - i - 1) + " шт!!!!.");
+                }
+            }
+        }
+    }
+}
